01385aaaa42bc1fe88aa0874774e14e151722dd2
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-21 08:06:36 +0300

    Entity Framework added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Business.Concrete;
-
+using DataAccess.EntityFramework;
 using System;
 
 namespace ConsoleUI
@@ -7,7 +7,16 @@ namespace ConsoleUI
     class Program
     {
         static void Main(string[] args)
+
         {
+            DatabaseContext databaseContex = new DatabaseContext();
+            foreach (var product in DatabaseContext.Cars)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+
             ProductManager productManager = new ProductManager(new DataAccess.EntityFramework.EfProductDal());
             foreach (var product in productManager.GetAll())
             {
--- DataAccess/Concrete/EntityFramework/DatabaseContext.cs
+++ DataAccess/Concrete/EntityFramework/DatabaseContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.EntityFramework
 {
-    public class DatabaseContext
+    public class DatabaseContext :DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(MSSQLLocaldb;Database=RentACar;Trusted_Connection=true");
+
+        }
+        public static DbSet<Product> Cars { get; set; }
     }
 }
