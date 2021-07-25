bed2985227eb72432f02ef2074a4eae8b612a9e4
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-25 10:24:26 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +10,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
 
-        { 
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll())
-            {
-                Console.WriteLine(product.ProductName);
-            }
+        {
+            //ProductManager productManager = new ProductManager(new EfProductDal());
+            //foreach (var product in productManager.GetAll())
+            //{
+            //   Console.WriteLine(product.ProductName);
+            //}
+            UserManager userManager = new UserManager(new EfUserDal());
+            User user1 = new User();
+            user1.FirstName = "Dilan";
+            user1.LastName = "Kaplan";
+            user1.Email = "dilankaplan@gmail.com";
+            user1.Password = "12345";
+
+            User user2 = new User();
+            user2.FirstName = "Alp";
+            user2.LastName = "Uğurlu";
+            user2.Email = "alpugurlu@gmail.com";
+            user2.Password = "17021998";
+
+
+
+
+            
         }
     }
 }
--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -10,7 +10,7 @@ namespace Entities.Concrete
     {
         public int UserId { get; set; }
         public string FirstName { get; set; }
-        public int LastName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
